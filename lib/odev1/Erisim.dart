ed832f85a0019672dfe623cf4cff843f0c3f0b09
omerkorkmz <97345262+omerkorkmz@users.noreply.github.com>
2023-03-25 12:32:34 +0300

    Add files via upload

---
+++ lib/odev1/Erisim.dart
@@ -0,0 +1,9 @@
+import 'package:project/odev/convertFahrenhiet.dart';
+
+void main(){
+
+  var erisim = Convert();
+   String a = erisim.cnv(50);
+   print(a);
+
+}
---
+++ lib/odev1/convertFahrenhiet.dart
@@ -0,0 +1,9 @@
+class Convert{
+
+  String cnv(double sicaklikDegeri){
+
+    return "$sicaklikDegeri derece = ${(sicaklikDegeri*1.8)+32} fahrenhiet";
+  }
+
+
+}
